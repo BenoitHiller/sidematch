a331b957c05d1db453c8c7c7bc9d9ab629b8a612
Benoit Hiller <benoit.hiller@gmail.com>
2015-01-23 22:37:52 -0500

    adding colour tests

--- test/test_pdiff.rb
+++ test/test_pdiff.rb
@@ -18,11 +18,39 @@ class TestParallelComparator < Test::Unit::TestCase
     end
   end
 
+  GREEN="\033[0;32m"
+  RED="\033[1;31m"
+  PURPLE="\033[0;35m"
+  CLEAR="\033[0m"
+
   def test_no_color
-    check_strings(["-C"],
+    check_strings(["-C", "-q"],
                   ["1\n2\n3\n", "1\n2\n4\n", "+ 1 1\n+ 2 2\n- 3 4\n",1],
                   ["1\n2\n3\n", "1\n2\n3\n", "+ 1 1\n+ 2 2\n+ 3 3\n",0],
                   ["1\n2\n3\n", "1\n2\n", "+ 1 1\n+ 2 2\n- 3 EOF\n",1],
                  ) 
   end
+
+  def test_simple
+    check_strings(["-c", "-q"],
+                  ["1\n2\n3\n", "1\n2\n4\n", "1 #{GREEN}1#{CLEAR}\n2 #{GREEN}2#{CLEAR}\n3 #{RED}4#{CLEAR}\n",1],
+                  ["1\n2\n3\n", "1\n2\n3\n", "1 #{GREEN}1#{CLEAR}\n2 #{GREEN}2#{CLEAR}\n3 #{GREEN}3#{CLEAR}\n",0],
+                  ["1\n2\n3\n", "1\n2\n", "1 #{GREEN}1#{CLEAR}\n2 #{GREEN}2#{CLEAR}\n3 #{PURPLE}EOF#{CLEAR}\n",1],
+                 ) 
+  end
+
+  def test_filter
+    check_strings(["-c", "-q", "-m"],
+                  ["1\n2\n3\n", "1\n2\n4\n", "1 #{GREEN}1#{CLEAR}\n2 #{GREEN}2#{CLEAR}\n",1],
+                  ["1\n2\n3\n", "1\n2\n3\n", "1 #{GREEN}1#{CLEAR}\n2 #{GREEN}2#{CLEAR}\n3 #{GREEN}3#{CLEAR}\n",0],
+                  ["1\n2\n3\n", "1\n2\n", "1 #{GREEN}1#{CLEAR}\n2 #{GREEN}2#{CLEAR}\n",1],
+                 ) 
+    check_strings(["-c", "-q", "-M"],
+                  ["1\n2\n3\n", "1\n2\n4\n", "3 #{RED}4#{CLEAR}\n",1],
+                  ["1\n2\n3\n", "1\n2\n3\n", "",0],
+                  ["1\n2\n3\n", "1\n2\n", "3 #{PURPLE}EOF#{CLEAR}\n",1],
+                 ) 
+
+  end
+
 end
